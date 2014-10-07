d80e39d69e280b778340f68afb842fe3d421997a
eroncevich <eroncevich@gmail.com>
2014-10-07 13:40:22 -0600

    Added scoring and Copy

--- gerrymander.py
+++ gerrymander.py
@@ -1,7 +1,7 @@
 import sys
 import string
+from copy import copy, deepcopy
 M = []
-D =[]
 m=0
 n=0
 
@@ -23,24 +23,25 @@ def main():
   print M
   m = len(M)
   n = len(M[0])
-  initDMatrix()
-  print D
-  createTree()
+  D = initDMatrix()
+  createTree(D)
 
 def initDMatrix():
   global m
   global n
+  D =[]
   for i in range(0,m):
     D.append([])
     for j in range(0,n):
-      D[i].append(0)
+      D[i].append(-1)
+  return D
 
-def createTree():
+def createTree(D):
   global m
   global n
   finished = False
   pNode = Node(None)
-  iterate(D,pNode,0)
+  iterate(D,pNode,-1)
 
 def iterate(D, pNode, num):
   global m
@@ -49,10 +50,10 @@ def iterate(D, pNode, num):
 
   #Check Vert Lines
   for j in range(0,n):
-    if(vertLine(j)):
+    if(vertLine(D,j)):
       cNode = Node(pNode)
       pNode.addChild(cNode)
-      newD = D
+      newD = deepcopy(D)
       for i in range(0,m):
         newD[i][j] = num+1
       iterate(newD,cNode,num+1)
@@ -60,31 +61,56 @@ def iterate(D, pNode, num):
 
   #Check Horz Lines
   for i in range(0,m):
-    if(horzLine(i)):
+    if(horzLine(D,i)):
       cNode = Node(pNode)
       pNode.addChild(cNode)
-      newD = D
+      newD = deepcopy(D)
       for j in range(0,n):
         newD[i][j] = num+1
       iterate(newD,cNode,num+1)
       finished = False
 
   if finished:
+    print score(D,num)
     print D
+  #print D
   return
 
-def vertLine(j):
+def vertLine(D,j):
   global m
   for i in range(0,m):
-    if(D[i][j]!=0):
+    if(D[i][j]!=-1):
       return False
   return True
-def horzLine(i):
+def horzLine(D,i):
   global n
   for j in range(0,n):
-    if(D[i][j]!=0):
+    if(D[i][j]!=-1):
       return False
   return True
+
+def score(D,num):
+  global m
+  global n
+  scoreD = 0
+  scoreR =0
+  nDem = [0]*(num+1)
+  nRep = [0]*(num+1)
+  for i in range(0,m):
+    for j in range(0,n):
+      if(D[i][j] <0):
+        continue
+      if M[i][j] =='D':
+        nDem[D[i][j]] = nDem[D[i][j]]+1
+      else:
+        nRep[D[i][j]] = nRep[D[i][j]]+1
+  for i in range(0,num+1):
+    if(nDem[i]>nRep[i]):
+      scoreD = scoreD+1
+    elif(nDem[i]<nRep[i]):
+      scoreR = scoreR+1
+  return scoreD-scoreR
+
 
 class Node:
   def __init__(self,parentN):
